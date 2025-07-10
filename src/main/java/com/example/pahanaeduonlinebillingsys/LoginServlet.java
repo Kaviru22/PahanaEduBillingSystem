263c46287c05aca4d45a2706b4fe6b394b307a40
kaviruMendis <kmendis2021@gmail.com>
2025-07-10 17:23:13 +0530

    Initial Commit - Add login page, signup page

---
+++ src/main/java/com/example/pahanaeduonlinebillingsys/LoginServlet.java
@@ -0,0 +1,27 @@
+package com.example.pahanaeduonlinebillingsys;
+
+import java.io.*;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        if ("admin".equals(username) && "admin123".equals(password)) {
+            resp.sendRedirect("admin.jsp");
+
+        } else if ("user".equals(username) && "user123".equals(password)) {
+            resp.sendRedirect("user.jsp");
+        }
+        else {
+            resp.sendRedirect("login.jsp?error=1");
+        }
+    }
+
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -2,12 +2,12 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>JSP - Hello World</title>
+    <title>Pahana Edu</title>
 </head>
 <body>
-<h1><%= "Hello World!" %>
+<h1><%= "Hello Test World!" %>
 </h1>
 <br/>
-<a href="hello-servlet">Hello Servlet</a>
+<a href="login-servlet">Hello Servlet</a>
 </body>
 </html>
