a68ba117a1103459101b65b1150419b6d5e3e1c2
kaviruMendis <kmendis2021@gmail.com>
2025-08-03 00:27:29 +0530

    Items Delete pages Done

--- src/main/java/com/example/pahanaeduonlinebillingsys/items/controller/DeleteItem.java
+++ src/main/java/com/example/pahanaeduonlinebillingsys/items/controller/DeleteItem.java
@@ -1,4 +1,30 @@
 package com.example.pahanaeduonlinebillingsys.items.controller;
 
-public class DeleteItem {
+import com.example.pahanaeduonlinebillingsys.customer.model.CustomerDelete;
+import com.example.pahanaeduonlinebillingsys.customer.service.CustomerDeleteService;
+import com.example.pahanaeduonlinebillingsys.items.model.ItemDelete;
+import com.example.pahanaeduonlinebillingsys.items.service.ItemDeleteService;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+
+    @WebServlet("/DeleteItem")
+    public class DeleteItem extends HttpServlet {
+
+        private final ItemDeleteService itemDeleteService = new ItemDeleteService();
+
+        protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            String itemno = req.getParameter("itemno");
+            ItemDelete itemDelete = new ItemDelete(itemno);
+
+            String result = itemDeleteService.deleteItem(itemDelete);
+            req.setAttribute("message", result);
+            req.getRequestDispatcher("DeleteItem.jsp").forward(req, resp);
+
+        }
 }
--- src/main/java/com/example/pahanaeduonlinebillingsys/items/model/ItemDelete.java
+++ src/main/java/com/example/pahanaeduonlinebillingsys/items/model/ItemDelete.java
@@ -1,4 +1,19 @@
 package com.example.pahanaeduonlinebillingsys.items.model;
 
 public class ItemDelete {
+
+    private String itemno;
+
+   public ItemDelete(String itemno) {
+       this.itemno = itemno;
+   }
+
+   public String getItemno() {
+       return itemno;
+   }
+
+   public void setItemno(String itemno) {
+       this.itemno = itemno;
+   }
+
 }
